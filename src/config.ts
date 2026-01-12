1120bee31c98f328b18e2a285010f4656083ce1b
二叉树树 <acofork@qq.com>
2026-01-12 19:43:36 +0800

    upgrade: 更新 config,ts 中的状态页面地址

--- src/config.ts
+++ src/config.ts
@@ -88,7 +88,7 @@ export const navBarConfig: NavBarConfig = {
 		},
 		{
 			name: "监控",
-			url: "https://vps.2x.nz", // Internal links should not include the base path, as it is automatically added
+			url: "https://status.acofork.com", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
